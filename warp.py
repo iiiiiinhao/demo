b45264ac5d67ece5ad45ade927a2dbba0efe2a38
iiiiiinhao <52751547+iiiiiinhao@users.noreply.github.com>
2021-08-08 19:36:09 +0800

    Update warp.py

--- warp.py
+++ warp.py
@@ -15,25 +15,22 @@ class Warper3d(nn.Module):
         super(Warper3d, self).__init__()
         """
         warp an image, according to the optical flow
-        image: [B, 1, D, H, W] image for sampling
-        flow: [B, 3, D, H, W] flow predicted from source image pair
+        image: [B, 1, H, W, D] image for sampling
+        flow: [B, 3, H, W, D] flow predicted from source image pair
         """
         self.img_size = img_size
 
-
-
         H, W, D =img_size
         print(H,W,D)
-
-
+        
+        #create a grid
         xx = torch.arange(0, H).view(-1, 1, 1).repeat(1, W, D)
         yy = torch.arange(0, W).view(1, -1, 1).repeat(H, 1, D)
         zz = torch.arange(0, D).view(1, 1, -1).repeat(H, W, 1)
 
-
-
         self.grid = np.zeros([1, H, W, D, 3])
 
+        #Normalized，same with the way of the training image set
         xx1 = 2.0 * xx / H - 1  # max(W-1,1)
         yy1 = 2.0 * yy / W - 1  # max(H-1,1)
         zz1 = 2.0 * zz / D - 1  # max(H-1,1)
@@ -49,21 +46,15 @@ class Warper3d(nn.Module):
         grid = self.grid
         grid = torch.tensor(grid)
 
-
-
         if img.is_cuda:
             grid = grid.cuda()
 
-
+        #Add grid and deformation field data
         flow = flow.permute(0, 2, 3, 4, 1)  # [bs, D, H, W, 3]
-
         vgrid = grid + flow
-
-
         img = img.to(torch.float64)
 
-
-
+        #Re-sampling
         output = F.grid_sample(img, vgrid, padding_mode='border', align_corners=True)  # , mode='nearest'
 
         return output
